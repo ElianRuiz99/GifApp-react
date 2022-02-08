4f1eb632cf5ddce59e5741b5bbc7848176633a35
Elian Ruiz <elianruizpedraza@gmail.com>
2022-02-08 10:10:34 -0500

    Se deja listo el ambiente y se crea el componente GitApp.js

---
+++ src/GifApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//FC
+const GifApp = () => {
+
+    return (
+        <>
+            <h2>GifApp</h2>
+            <hr />
+        </>
+    );
+}
+
+export default GifApp;
