915e0c1e6e8e82cca8adac92b8c65fc83240e606
Elian Ruiz <elianruizpedraza@gmail.com>
2022-02-09 12:00:39 -0500

    Se crea un componente llamado GifGridItem donde decibe la data y esta la muestra

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
+    //useEffect permite ejecutar un codigo de forma condicional
+    useEffect( () => {
+        getGifs();
+    }, [] );
+
     const getGifs = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Simpsons&limit=15&api_key=t72iykm5f4sVEg6N4KTrELCwv2h3wgK0';
-    
+
         const resp = await fetch( url );
         //Extraer informacion 
         const { data } = await resp.json();
@@ -16,12 +24,20 @@ export const GifGrid = ({ category }) => {
             }
         } );
         console.log(gifs);
+        setImages(gifs);
     }   
 
-    getGifs();
     return (
         <div>
             <h3> { category } </h3>
+            {
+                images.map( img => (
+                    <GifGridItem 
+                        key={ img.id }
+                        { ...img }
+                    />
+                ) )
+            }
         </div>
     )
 };
